d7910775b1f9a454c7bd67ad88dd90b1e2bf9563
Iago Nobre Silva <iago.nobre@academico.ifrn.edu.br>
2022-01-20 11:26:34 -0300

    Create: Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,48 @@ import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.colors}>Hello World!</h1>
+    <main>
+      <section className={styles.mainBackground}>
+        <div>
+          <header>
+            <div>
+              <a href="">
+                <img src="/assets/logo.svg" alt="Currículo na Mão" />
+              </a>
+            </div>
+            <div>
+              <button>
+                <img src="/assets/text-icon-plus.svg" alt="" />
+              </button>
+              <button>
+                <img src="/assets/text-icon.svg" alt="" />
+              </button>
+              <button>
+                <img src="/assets/text-icon-less.svg" alt="" />
+              </button>
+              <button>
+                <img src="/assets/contrast.svg" alt="" />
+              </button>
+              <button>CRIAR CONTA</button>
+              <button>ENTRAR</button>
+            </div>
+          </header>
+          <div className={styles.createAccount}>
+            <div>
+              <h1>Crie seu currículo de forma fácil, acessível e gratuita!</h1>
+              <p>Preencha suas informações de modo prático e gere seu currículo no modelo de sua escolha.</p>
+              <button>CRIAR MEU CURRÍCULO</button>
+              <a href="">
+                <img src="/assets/arrow-down.svg" alt="" />
+                <p>Saiba Mais</p>
+              </a>
+            </div>
+            <div>
+              <img src="/assets/woman-image.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
