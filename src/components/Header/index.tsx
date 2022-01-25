785c3ea7107fdfd6084f18a5896de9e05b7c0f19
Iago Nobre Silva <iago.nobre@academico.ifrn.edu.br>
2022-01-25 11:16:51 -0300

    Create: Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
+import styles from './style.module.scss';
+
 function Header() {
-   return (
-     <header>
-       
-     </header>
-   )
+  return (
+    <header className={styles.headerStyle}>
+      <div>
+        <Link href="/">
+          <a>
+          <img src="/assets/logo.svg" alt="Currículo na Mão" />
+          </a>
+        </Link>
+      </div>
+      <div>
+        <div>
+          <button className={styles.iconButton}>
+            <img src="/assets/text-icon-plus.svg" alt="Aumentar Textos" />
+          </button>
+          <button className={styles.iconButton}>
+            <img src="/assets/text-icon.svg" alt="Textos em tamanho padrão" />
+          </button>
+          <button className={styles.iconButton}>
+            <img src="/assets/text-icon-less.svg" alt="Diminuir Textos" />
+          </button>
+          <button className={styles.iconButton}>
+            <img src="/assets/contrast.svg" alt="Contraste" />
+          </button>
+        </div>
+      </div>               
+    </header>
+  )
 }
