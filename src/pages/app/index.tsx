738c0b523ab524fa797f862fe8737641ecf66f5c
Liriel Antônia de Lima Felix <liriel.felix@academico.ifrn.edu.br>
2022-01-26 11:06:58 -0300

    Update App

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,14 +1,18 @@
 import styles from './home.module.scss';
 
 import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
 
 export default function HomeApp() {
   return (
     <>
-      <Header></Header>
-      <div>
-        <p>Você ainda o possui <br />um currículo</p>
-        <img src="/assets/homeapp.svg" alt="" />
+      <Header />
+      <div className={styles.nothingContainer}>
+        <div>
+          <p>Você ainda o possui <br />um currículo</p>
+          <img src="/assets/homeapp.svg" alt="Vizinhança vazia" />
+          <Button styleType="outline">CRIAR MEU CURRÍCULO</Button>
+        </div>
       </div>
     </>
   )
