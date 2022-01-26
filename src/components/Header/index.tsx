186f162086eb24eb92fdaaa12fafa9c94bdca5c8
Iago Nobre Silva <iago.nobre@academico.ifrn.edu.br>
2022-01-26 11:05:37 -0300

    Update: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,27 +5,34 @@ export function Header() {
   return (
     <header className={styles.headerStyle}>
       <div>
-        <Link href="/">
+        <Link href="/app">
           <a>
           <img src="/assets/logo.svg" alt="Currículo na Mão" />
           </a>
         </Link>
       </div>
       <div>
-        <div>
-          <button className={styles.iconButton}>
-            <img src="/assets/text-icon-plus.svg" alt="Aumentar Textos" />
-          </button>
-          <button className={styles.iconButton}>
-            <img src="/assets/text-icon.svg" alt="Textos em tamanho padrão" />
-          </button>
-          <button className={styles.iconButton}>
-            <img src="/assets/text-icon-less.svg" alt="Diminuir Textos" />
-          </button>
-          <button className={styles.iconButton}>
-            <img src="/assets/contrast.svg" alt="Contraste" />
-          </button>
-        </div>
+        <button className={styles.iconButton}>
+          <img src="/assets/text-icon-plus.svg" alt="Aumentar Textos" />
+        </button>
+        <button className={styles.iconButton}>
+          <img src="/assets/text-icon.svg" alt="Textos em tamanho padrão" />
+        </button>
+        <button className={styles.iconButton}>
+          <img src="/assets/text-icon-less.svg" alt="Diminuir Textos" />
+        </button>
+        <button className={styles.iconButton}>
+          <img src="/assets/contrast.svg" alt="Contraste" />
+        </button>
+        <details>
+          <summary className={styles.dropdownTrigger}>
+            <img src="/assets/profile.png" alt=""/>
+          </summary>
+          
+          <div className={styles.dropdownContent}>
+            Dropdown content here…
+          </div>
+        </details>
       </div>               
     </header>
   )
