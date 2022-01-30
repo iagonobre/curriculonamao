be52c2abd17ce527547377297c79d67111b6f1d9
Iago Nobre <iagonobre22@gmail.com>
2022-01-30 12:27:44 -0300

    Update Perfil

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -4,16 +4,23 @@ import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 
 export default function HomeApp() {
+  const haveCurriculum = true;
+
   return (
     <>
       <Header />
-      <div className={styles.nothingContainer}>
-        <div>
-          <p>Você ainda o possui <br />um currículo</p>
-          <img src="/assets/homeapp.svg" alt="Vizinhança vazia" />
-          <Button styleType="outline">CRIAR MEU CURRÍCULO</Button>
+      {haveCurriculum ? (
+        <div></div>
+      ) : (
+        <div className={styles.nothingContainer}>
+          <div>
+            <p>Você ainda o possui <br />um currículo</p>
+            <img src="/assets/homeapp.svg" alt="Vizinhança vazia" />
+            <Button styleType="outline">CRIAR MEU CURRÍCULO</Button>
+          </div>
         </div>
-      </div>
+      )}
+
     </>
   )
 }
---
+++ src/pages/app/perfil/index.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import { FiArrowLeft } from 'react-icons/fi';
+import { AcessibilityScroll } from '../../../components/AccessibilityScroll';
+import { Button } from '../../../components/Button';
+import { Input } from '../../../components/Input';
+import styles from './perfil.module.scss';
+
+
+export default function Perfil() {
+  return (
+    <>
+      <AcessibilityScroll />
+      <div className={styles.container}>
+        <div className={styles.boxContent}>
+          <div className={styles.profileForm}>
+            <FiArrowLeft size={20} />
+            <div className={styles.imageContainer}>
+              <img src="/assets/profile2.png" alt="" />
+              <button>Alterar</button>
+            </div>
+            <Input type="text" placeholder="Nome completo" value="Iago Nobre Silva" />
+            <Input type="email" placeholder="Seu e-mail" value="iagonobre22@gmail.com" />
+            <Button>EDITAR</Button>
+          </div>
+          <div className={styles.line} />
+          <div className={styles.profileContent}>
+            <h3>Editar Perfil</h3>
+            <div />
+            <Link href=""><a>Alterar minha senha</a></Link>
+            <Link href=""><a>Política de Privacidade</a></Link>
+            <Link href=""><a>Excluir minha conta</a></Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
